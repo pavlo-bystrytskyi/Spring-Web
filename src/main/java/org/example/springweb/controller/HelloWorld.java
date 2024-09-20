180f13f5d0e5b8b09dbf0a672015023122806255
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-20 10:10:13 +0200

    Hello by name action added.

--- src/main/java/org/example/springweb/controller/HelloWorld.java
+++ src/main/java/org/example/springweb/controller/HelloWorld.java
@@ -1,13 +1,18 @@
 package org.example.springweb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/hello")
 public class HelloWorld {
 
-    @GetMapping("/api/hello")
+    @GetMapping
     public String sayHello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/{name}")
+    public String sayHelloToSomeone(@PathVariable("name") String name) {
+        return "Hello, " + name + "!";
     }
 }
